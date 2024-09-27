1d5a89128d804ad0c858c1ddeb9ed40d99a02ae2
yousef-alhabbash <yousef.al-habbash@mths.ca>
2024-09-27 11:25:40 -0400

    I changed the code to a code that can turn on and off a microbit

--- main.ts
+++ main.ts
@@ -1,8 +1,23 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Mr. Yousef
+ * Created on: Sep 2024
+ * This program turns led on and off using microbit.
 */
 
-basic.showString('Hello, World!')
+
+basic.clearScreen()
+pins.digitalWritePin(DigitalPin.P16, 0)
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Yes)
+    pins.digitalWritePin(DigitalPin.P16, 1)
+})
+
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.No)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+})
+
+basic.showString('Yousef')
